1d4b20c28a2ee618a8b6ed16d2cb5e43b28e7a3b
Duncan Haig <duncanhaig@btopenworld.com>
2016-10-03 19:32:56 +0100

    welcome screen cleaned up

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'M2010336\'s Advanced Server-side Technology Project',
 
     /*
     |--------------------------------------------------------------------------
