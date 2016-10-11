ce43630474475f61d620f9089969543ca0a4f95b
Duncan Haig <duncanhaig@btopenworld.com>
2016-10-11 20:28:33 +0100

    scss compilation added, styles changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Leafo\ScssPhp\Compiler;
 
 class HomeController extends Controller
 {
@@ -25,4 +26,17 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function css() {
+      $css;
+      $scss = new Compiler();
+      $scss->addImportPath(function($path) {
+        $basePath = base_path("resources/assets/sass/");
+          if (!file_exists($basePath.$path)) return null;
+          return $basePath.$path;
+      });
+      $scss->setFormatter('Leafo\ScssPhp\Formatter\Crunched');
+      $css = $scss->compile('@import "app.scss";');
+      return response($css)->header('Content-Type', 'text/css');
+      }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/css', 'HomeController@css');
 Route::get('/home', 'HomeController@index');
