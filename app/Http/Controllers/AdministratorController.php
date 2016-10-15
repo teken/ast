a5e491a497cb5a3e000d7350493f1de9597acabb
Duncan Haig <duncanhaig@btopenworld.com>
2016-10-15 17:59:51 +0100

    middleware checks in place

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class AdministratorController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('admin');
+    }
+
     public function index(Request $request) {
 
       return view('admin.index');
