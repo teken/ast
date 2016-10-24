17c5cb160e91fe4ab33e2d4b1a022323306b3639
HAIG <M2010336@live.tees.ac.uk>
2016-10-24 15:19:06 +0100

    cba to explain

--- resources/views/video/bymodule.blade.php
+++ resources/views/video/bymodule.blade.php
@@ -1,22 +1,18 @@
-@extends('layouts.app')
-
-@section('content')
-  @forelse ($modules as $module)
-    @if (count($module->videos()) > 0)
-      <div class="row">
-        <div class="title">
-          {{ $module->title }}
-        </div>
-        <div class="videos">
-          @each('video.box', $module->videos(), 'video')
-        </div>
-      </div>
-    @endif
-  @empty
+@forelse ($modules as $module)
+  @if (count($module->videos()) > 0)
     <div class="row">
-      <div class="message">
-        Sorry we didn't find any videos here, you could try adding some.
+      <div class="title">
+        {{ $module->title }}
+      </div>
+      <div class="videos">
+        @each('video.box', $module->videos(), 'video')
       </div>
     </div>
-  @endforelse
-@endsection
+  @endif
+@empty
+  <div class="row">
+    <div class="message">
+      Sorry we didn't find any videos here, you could try adding some.
+    </div>
+  </div>
+@endforelse
