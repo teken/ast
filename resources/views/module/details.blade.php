96fe1902debea7384eefbdd758702a7e847ce0cb
Laptop <duncanhaig@btopenworld.com>
2016-10-31 16:52:30 +0000

    more actions moved

--- resources/views/module/details.blade.php
+++ resources/views/module/details.blade.php
@@ -1,18 +1,10 @@
 @extends('layouts.app')
 
+@section('adminactions')
+  <li><a href="{{url('/modules/'.$module->slug.'/edit')}}">Edit Module</a></li>
+@endsection
+
 @section('content')
-    @if(!Auth::guest() and Auth::user()->administrator)
-        <div class="row admin toolbar">
-          <div class="btn-group pull-right">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="glyphicon glyphicon-cog"></i><span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu">
-              <li><a href="{{url('/modules/'.$module->slug.'/edit')}}">Edit Module</a></li>
-            </ul>
-          </div>
-        </div>
-    @endif
     <div class="module details">
       <div class="title">{{$module->title}}</div>
       <div class="description">{{$module->description}}</div>
--- resources/views/video/details.blade.php
+++ resources/views/video/details.blade.php
@@ -1,38 +1,30 @@
 @extends('layouts.app')
 
+@section('adminactions')
+  <li><a href="{{url("/videos/{$video->slug}/edit")}}">Edit Video</a></li>
+@endsection
+
 @section('content')
-    @if(!Auth::guest() and Auth::user()->administrator)
-        <div class="row admin toolbar">
-          <div class="btn-group pull-right">
-            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="glyphicon glyphicon-cog"></i><span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu">
-              <li><a href="{{url("/videos/{$video->slug}/edit")}}">Edit Video</a></li>
-            </ul>
-          </div>
-        </div>
-    @endif
-    <div class="video details">
-      <div class="player wrapper">
-        <div data-type="{{$video->getVideoHost()}}" data-video-id="{{$video->getVideoId()}}"></div>
+  <div class="video details">
+    <div class="player wrapper">
+      <div data-type="{{$video->getVideoHost()}}" data-video-id="{{$video->getVideoId()}}"></div>
+    </div>
+    <div class="details">
+      <div class="title">{{$video->title}}</div>
+      <div class="url">{{$video->url}}</div>
+      <div class="description">{{$video->description}}</div>
+      <div class="tags">{{$video->tags}}</div>
+      <div class="actions">
+        @if(!Auth::guest() and Auth::user()->favourites()->pluck('id')->contains($video->id))
+          <a class="btn btn-default unfavourite" href="{{url("/videos/{$video->slug}/unfavourite")}}">Unfavourite</a>
+        @else
+          <a class="btn btn-default favourite" href="{{url("/videos/{$video->slug}/favourite")}}">Favourite</a>
+        @endif
       </div>
-      <div class="details">
-        <div class="title">{{$video->title}}</div>
-        <div class="url">{{$video->url}}</div>
-        <div class="description">{{$video->description}}</div>
-        <div class="tags">{{$video->tags}}</div>
-        <div class="actions">
-          @if(!Auth::guest() and Auth::user()->favourites()->pluck('id')->contains($video->id))
-            <a class="btn btn-default unfavourite" href="{{url("/videos/{$video->slug}/unfavourite")}}">Unfavourite</a>
-          @else
-            <a class="btn btn-default favourite" href="{{url("/videos/{$video->slug}/favourite")}}">Favourite</a>
-          @endif
-        </div>
-      </div>
-      <div class="comments">
-        <div class="new">
-          @if(!Auth::guest())
+    </div>
+    <div class="comments">
+      <div class="new">
+        @if(!Auth::guest())
           <form action="{{url("/videos/{$video->slug}/comment")}}" method="POST">
             {!! csrf_field() !!}
             {!! method_field('PUT') !!}
@@ -43,13 +35,13 @@
               <input class="btn btn-default" type="submit" value="Submit Comment"/>
             </div>
           </form>
-          @else
-            <textarea type="text" name="comment" placeholder="Please sign to in to comment." disabled></textarea>
-          @endif
-        </div>
-        @each('video.comment.box', $video->comments(), 'comment')
+        @else
+          <textarea type="text" name="comment" placeholder="Please sign to in to comment." disabled></textarea>
+        @endif
       </div>
+      @each('video.comment.box', $video->comments(), 'comment')
     </div>
+  </div>
 @endsection
 
 @section('scripts')
