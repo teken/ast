9e9511157a8adbc0f3a570b3d5952564f580e52e
Laptop <duncanhaig@btopenworld.com>
2016-10-31 15:06:25 +0000

    d

--- resources/views/video/bymodule.blade.php
+++ resources/views/video/bymodule.blade.php
@@ -1,6 +1,6 @@
 @forelse ($modules as $module)
   @if (count($module->videos()) > 0)
-    <div class="row">
+    <div class="module gallery">
       <div class="title">
         {{ $module->title }}
       </div>
@@ -10,7 +10,7 @@
     </div>
   @endif
 @empty
-  <div class="row">
+  <div class="module gallery empty">
     <div class="message">
       Sorry we didn't find any videos here, you could try adding some.
     </div>
