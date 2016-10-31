61c722957029ab954c07b12abf710061b83f794a
Laptop <duncanhaig@btopenworld.com>
2016-10-31 16:42:14 +0000

    removing row for places

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -14,7 +14,7 @@
         </div>
     @endif
     @foreach($courses as $course)
-        <div class="row course">
+        <div class="course gallery">
           <div class="title"><a href="{{url('/courses/'.$course->slug)}}">{{$course->title}}</a></div>
           <div class="modules">
             <ul>
