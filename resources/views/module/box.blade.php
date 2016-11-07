4d8c0a841a6455edd3260417afcbc9baff004afa
Laptop <duncanhaig@btopenworld.com>
2016-11-07 14:33:25 +0000

    ffs

--- resources/views/module/box.blade.php
+++ resources/views/module/box.blade.php
@@ -2,7 +2,7 @@
   <div class="wrapper">
     <a href="{{url('/modules/'.$module->slug)}}">
       <div class="title">{{$module->title}}</div>
-      @if($module->videos()->count > 0)
+      @if($module->videos()->count() > 0)
         <img class="thumb" href="{{$module->videos()->first()->slug}}" />
       @endif
     </a>
