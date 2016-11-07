852c4ef547db8fcd5b7a485b00c5aba3d45f8387
Laptop <duncanhaig@btopenworld.com>
2016-11-07 10:28:53 +0000

    gallery box styling

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -8,10 +8,15 @@
     @foreach($courses as $course)
         <div class="course gallery">
           <div class="title"><a href="{{url('/courses/'.$course->slug)}}">{{$course->title}}</a></div>
-          <div class="modules">
+          <div class="modules boxes">
             <ul>
               @foreach($course->modules()->get() as $module)
-                  <li><a href="{{url('/modules/'.$module->slug)}}">{{$module->title}}</a></li>
+                  <li>
+                    <div class="wrapper">
+                      <a href="{{url('/modules/'.$module->slug)}}">{{$module->title}}
+                      </div>
+                    </a>
+                  </li>
               @endforeach
             </ul>
           </div>
