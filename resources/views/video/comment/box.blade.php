78262b7561c8f763488b3e3f50fc3e68788d440d
Laptop <duncanhaig@btopenworld.com>
2016-11-07 15:06:29 +0000

    first added

--- resources/views/video/comment/box.blade.php
+++ resources/views/video/comment/box.blade.php
@@ -1,4 +1,4 @@
 <div class="comment">
-  <div class="name">{{$comment->user()->name()}}</div>
+  <div class="name">{{$comment->user()->first()->name()}}</div>
   <div class="text">{{$comment->comment}}</div>
 </div>
