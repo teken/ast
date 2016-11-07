96a4efb43d9f66611ca2a0e42ee466727e09635c
Laptop <duncanhaig@btopenworld.com>
2016-11-07 14:32:41 +0000

    module thumbs

--- resources/views/module/box.blade.php
+++ resources/views/module/box.blade.php
@@ -2,6 +2,9 @@
   <div class="wrapper">
     <a href="{{url('/modules/'.$module->slug)}}">
       <div class="title">{{$module->title}}</div>
+      @if($module->videos()->count > 0)
+        <img class="thumb" href="{{$module->videos()->first()->slug}}" />
+      @endif
     </a>
   </div>
 </div>
