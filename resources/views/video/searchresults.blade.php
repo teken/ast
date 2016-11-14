ae94cf10631d6b169a5f97dab96230575256293f
Laptop <duncanhaig@btopenworld.com>
2016-11-14 13:43:50 +0000

    search result error message

--- resources/views/video/searchresults.blade.php
+++ resources/views/video/searchresults.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="videos gallery">
-    @each('video.box', $videos, 'video')
-  </div>
+  @if($videos->count() > 0)
+    <div class="videos gallery">
+      @each('video.box', $videos, 'video')
+    </div>
+  @else
+    <div class="videos none">
+      <h1>Im sorry but i can't find any videos :(</h1>
+    </div>
+  @endif
 @endsection
