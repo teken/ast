2cd5966d7b2c4b3bad5b6f2ebba20fd0c24961de
Laptop <duncanhaig@btopenworld.com>
2016-11-28 12:16:35 +0000

    fixes

--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -29,7 +29,7 @@
                           $currentModules = $course->modules()->get();
                         ?>
                         @forelse($modules as $module)
-                          <option value="{{$module->id}}" @if($course->modules()->contains($module)) checked @endif>{{$module->title}}</option>
+                          <option value="{{$module->id}}" @if($currentModules->contains($module)) checked @endif>{{$module->title}}</option>
                         @empty
                           <option>Sorry there are no modules that i can find.</option>
                         @endforelse
