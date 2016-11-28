f434ce6b0fd9d819f2aedef6763b9dc2f6a27a19
Laptop <duncanhaig@btopenworld.com>
2016-11-28 11:58:32 +0000

    fixes

--- resources/views/video/index.blade.php
+++ resources/views/video/index.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-  @if (!Auth::guest())
-    <div class="title">Subscriptions</div>
-      <div class="videos gallery grid">
-        @each('video.box', $subscriptions, 'video')
-      </div>
-  @endif
-  <div class="title">New Videos</div>
-  <div class="videos gallery grid">
-    @each('video.box', $videos, 'video')
+  <div class="home">
+    @if (!Auth::guest())
+      <div class="title">Subscriptions</div>
+        <div class="videos gallery grid">
+          @each('video.box', $subscriptions, 'video')
+        </div>
+    @endif
+    <div class="title">New Videos</div>
+    <div class="videos gallery grid">
+      @each('video.box', $videos, 'video')
+    </div>
   </div>
 @endsection
