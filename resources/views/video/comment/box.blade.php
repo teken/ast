98dbf46844586209364c3fe8e49409216dff56d0
Laptop <duncanhaig@btopenworld.com>
2016-11-28 10:25:43 +0000

    comment drop down fixes

--- resources/views/video/comment/box.blade.php
+++ resources/views/video/comment/box.blade.php
@@ -2,15 +2,15 @@
   <div class="name">{{$comment->user()->first()->name()}}</div>
   <div class="text">{{$comment->comment}}</div>
   <div class="actions pull-right">
-    <div class="btn-group">
+    @if(!Auth::guest() and Auth::user()->administrator)
+    <div class="btn-group drop up">
       <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <i class="glyphicon glyphicon-cog"></i>
       </button>
       <ul class="dropdown-menu">
-        @if(!Auth::guest() and Auth::user()->administrator)
           <li><a href="{{url("/comments/{$comment->id}/delete")}}">Delete Comment</a></li>
-        @endif
       </ul>
     </div>
+    @endif
   </div>
 </div>
